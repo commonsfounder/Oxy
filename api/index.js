c20f3f989ce7711031d6f6486475ad29b87c5cba
commonsfounder <chizigamonyewuchi@gmail.com>
2026-05-04 09:06:24 +0100

    Serve static files (index.html) from Express app root

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..')));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
