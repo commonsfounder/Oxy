442a2f926181da3520f467976dc1d7fa4edbe20c
commonsfounder <chizigamonyewuchi@gmail.com>
2026-05-04 09:08:59 +0100

    Route API calls to api/index.js, let Vercel serve index.html as static

--- api/index.js
+++ api/index.js
@@ -14,7 +14,6 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..')));
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
