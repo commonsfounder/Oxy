f82e3bacd01e61fe3b1bbe26c53d3b0ce9e7f60f
Chizi <chizigamonyewuchi@gmail>
2026-05-24 15:49:45 +0100

    Initial Commit

---
+++ Oxy/OxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxyApp.swift
+//  Oxy
+//
+//  Created by Chizigam Onyewuchi on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct OxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
